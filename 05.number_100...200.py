3b57e08efdf105e89ace6dfb95928a58efad068b
Aleksandar <Sasho80@gbg.bg>
2024-05-03 14:48:03 +0300

    Add files via upload

---
+++ 05.number_100...200.py
@@ -0,0 +1,8 @@
+number_user = int(input())
+
+if number_user < 100:
+    print("Less than 100")
+elif 100 <= number_user <= 200:
+    print("Between 100 and 200")
+else:
+    print("Greater than 200")
